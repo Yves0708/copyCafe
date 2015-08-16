b8def780aa389fb58675a3255c3c11ecae032c4e
Yves0708 <yvesloveisis@gmail.com>
2015-08-16 16:03:57 +0800

    cafe1.1

--- Cafe for Student/src/net/cloud95/android/lession/cafe/MainActivity.java
+++ Cafe for Student/src/net/cloud95/android/lession/cafe/MainActivity.java
@@ -10,6 +10,13 @@ import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class MainActivity extends Activity {
+	private RadioGroup sizerg;
+	private RadioGroup icerg;
+	private RadioGroup sugarrg;
+	private RadioButton[] sizerb = new RadioButton[3];
+	private RadioButton[] icerb = new RadioButton[5];
+	private RadioButton[] sugarrb = new RadioButton[5];
+	private Button[] cafebtns = new Button[8];
 	//咖啡名稱
 	private String[] cafeName={
 			"美式咖啡",
@@ -32,15 +39,46 @@ public class MainActivity extends Activity {
 			{65,80,90},
 			{65,80,90}
 	};
+	private int radioButtonCheckedColor = Color.RED;
+	private int radioButtonUnCheckedColor = Color.BLACK;
 	//目前選擇的大小杯
+	private int sizeIndex =2 ;
 	//目前選擇的冰
+	private int iceIndex = 4;
 	//目前選擇的糖
+	private int sugarIndex = 4;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		processView();//處理視覺
+		processControl();//流程控制
 	}
 	private void processView(){
+		sizerg =(RadioGroup) findViewById(R.id.sizerg);
+		sugarrg =(RadioGroup) findViewById(R.id.sugarrg);
+		icerg =(RadioGroup) findViewById(R.id.icerg);
+		sizerb[0] = (RadioButton)findViewById(R.id.sizerb1);
+		sizerb[1] = (RadioButton)findViewById(R.id.sizerb2);
+		sizerb[2] = (RadioButton)findViewById(R.id.sizerb3);
+		sugarrb[0] =(RadioButton)findViewById(R.id.sugarrb1);
+		sugarrb[1] =(RadioButton)findViewById(R.id.sugarrb2);
+		sugarrb[2] =(RadioButton)findViewById(R.id.sugarrb3);
+		sugarrb[3] =(RadioButton)findViewById(R.id.sugarrb4);
+		sugarrb[4] =(RadioButton)findViewById(R.id.sugarrb5);
+		icerb[0] = (RadioButton)findViewById(R.id.icerb1);
+		icerb[1] = (RadioButton)findViewById(R.id.icerb2);
+		icerb[2] = (RadioButton)findViewById(R.id.icerb3);
+		icerb[3] = (RadioButton)findViewById(R.id.icerb4);
+		icerb[4] = (RadioButton)findViewById(R.id.icerb5);
+		cafebtns[0] =(Button) findViewById(R.id.cafebtn1);
+		cafebtns[1] =(Button) findViewById(R.id.cafebtn2);
+		cafebtns[2] =(Button) findViewById(R.id.cafebtn3);
+		cafebtns[3] =(Button) findViewById(R.id.cafebtn4);
+		cafebtns[4] =(Button) findViewById(R.id.cafebtn5);
+		cafebtns[5] =(Button) findViewById(R.id.cafebtn6);
+		cafebtns[6] =(Button) findViewById(R.id.cafebtn7);
+		cafebtns[7] =(Button) findViewById(R.id.cafebtn8);
 	}
 	
 	private void processControl(){
